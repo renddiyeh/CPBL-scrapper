f5db71649748779f686fd802d7afc78f1dcd449e
Renddi Yeh <renddi@relab.cc>
2021-12-30 19:02:13 +0800

    init

---
+++ convertJson.py
@@ -0,0 +1,64 @@
+import os
+import json
+import csv
+from pathlib import Path
+
+def getTeamStat(scoreBoard, abbr, key):
+    cnt = 0
+    for score in scoreBoard:
+        if score['TeamAbbr'] == abbr:
+            cnt += score[key]
+    return cnt
+
+def sortGameFile(fielName):
+    name = Path(fielName).stem
+    return int(name)
+
+years = os.listdir('data')
+teamPreifixes = ['Home', 'Visiting']
+
+mainKeys = [
+    'Year',
+    'GameSno',
+    'HomeTeamName',
+    'VisitingTeamName',
+    'HomeTotalScore',
+    'VisitingTotalScore',
+]
+calcKeys = [
+    'HittingCnt',
+    'ErrorCnt',
+]
+header = mainKeys.copy()
+for teamPrefix in teamPreifixes:
+    for key in calcKeys:
+        header.append(teamPrefix + key)
+header.append('InningCnt')
+
+rows = []
+for year in years:
+    dirname = 'data/' + year
+    gameFiles = os.listdir(dirname)
+    gameFiles.sort(key = sortGameFile)
+    for gameFile in gameFiles:
+        f = open(dirname + '/' + gameFile, encoding='utf-8')
+        # returns JSON object as
+        # a dictionary
+        data = json.load(f)
+        row = []
+        for key in mainKeys:
+            row.append(data['CurtGameDetailJson'][key])
+        for teamPrefix in teamPreifixes:
+            for key in calcKeys:
+                teamName = data['CurtGameDetailJson'][teamPrefix + 'TeamName']
+                row.append(getTeamStat(data['ScoreboardJson'], teamName, key))
+
+        row.append(data['ScoreboardJson'][0]['InningSeq'])
+        rows.append(row)
+
+print(len(rows))
+with open('GameStats.csv', 'w', newline='', encoding='utf-8') as f:
+    # using csv.writer method from CSV package
+    write = csv.writer(f)
+    write.writerow(header)
+    write.writerows(rows)
---
+++ main.py
@@ -0,0 +1,89 @@
+from bs4 import BeautifulSoup
+import json
+import requests
+import os
+
+years = {
+    "2021": 300,
+    "2020": 240,
+    "2019": 240,
+    "2018": 240,
+    "2017": 240,
+    "2016": 240,
+    "2015": 240,
+    "2014": 240,
+    "2013": 240,
+    "2012": 240,
+    "2011": 240,
+    "2010": 240,
+    "2009": 240,
+    "2008": 300,
+    "2007": 300,
+    "2006": 300,
+    "2005": 300,
+    "2004": 300,
+    "2003": 300,
+    "2002": 180,
+    "2001": 180,
+    "2000": 180,
+    "1999": 278,
+    "1998": 315,
+    "1997": 336,
+    "1996": 300,
+    "1995": 300,
+    "1994": 270,
+    "1993": 270,
+    "1992": 180,
+    "1991": 180,
+    "1990": 180,
+}
+
+def getGameData(year, gameNo):
+    session = requests.Session()
+    response = session.get('https://www.cpbl.com.tw/box/index', verify = False)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    token = soup.find(attrs={"name": "__RequestVerificationToken"})
+
+    params = {
+        "__RequestVerificationToken": token,
+        "GameSno": gameNo,
+        "KindCode": "A",
+        "Year": year,
+    }
+
+    def parse_json(d):
+        for k in d:
+            if isinstance(d[k], str):
+                d[k] = json.loads(d[k])
+        return d
+
+    try:
+        response = session.post('https://www.cpbl.com.tw/box/getlive', params = params, verify = False)
+
+        jsonData = response.json()
+        dir = 'data/' + str(year)
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
+        with open(dir + '/' + str(gameNo) + '.json', 'w', encoding='utf8') as f:
+            json.dump(parse_json(jsonData), f, ensure_ascii=False)
+    except:
+        print('error for ' + str(year) + ' game ' + str(gameNo))
+
+
+def getYearData(year):
+    games = years[str(year)]
+    for game in range(games):
+        getGameData(year, game + 1)
+
+toDo = [[1999, 189], [2008,257]]
+
+for task in toDo:
+    year = task[0]
+    start = task[1]
+    games = years[str(year)]
+    for game in range(start, games):
+        getGameData(year, game + 1)
+
+# for year in range(2009, 2022):
+#     getYearData(year)
