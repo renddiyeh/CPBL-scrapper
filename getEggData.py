b419b2bfb33a2bc62abafd72a4832abe77e06907
Renddi Yeh <renddi@relab.cc>
2022-02-21 17:30:43 +0800

    more egg prices

--- getEggData.py
+++ getEggData.py
@@ -1,3 +1,4 @@
+from turtle import st
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -7,11 +8,17 @@ def getEggData(d):
     response = requests.get('https://tw.chinyieggs.com/egg_detail/' + str(d) + '/')
     soup = BeautifulSoup(response.text, 'html.parser')
     date = soup.select('body > div.wrapper > div.inside > div > div.R > div > table > tbody > tr:nth-child(1) > th')[0].text
-    priceTxt = soup.select('body > div.wrapper > div.inside > div > div.R > div > table > tbody > tr:nth-child(4) > td:nth-child(2)')[0].text
-    x = re.search("\d+", priceTxt)
-    price = x.group()
-    print(date + ': ' + price)
-    return [date, price]
+    
+    prices = []
+    for n in range(4, 7):
+        priceEle = soup.select('body > div.wrapper > div.inside > div > div.R > div > table > tbody > tr:nth-child(' + str(n) + ') > td:nth-child(2)')[0]
+        priceTxt = priceEle.get_text(strip=True, separator='\n').splitlines()
+        if n > 4:
+            prices.append(priceTxt[1])
+        else:
+            x = re.search("\d+", priceTxt[0])
+            prices.append(x.group())
+    return [date] + prices
 
 all_prices = []
 for d in range(200, 318):
@@ -20,5 +27,5 @@ for d in range(200, 318):
 
 with open('eggPrice.csv', 'w', encoding='utf-8', newline='') as f:
     write = csv.writer(f)
-    write.writerow(['date', 'price'])
+    write.writerow(['date', '雞蛋', '洗選蛋', 'CAS'])
     write.writerows(all_prices)
